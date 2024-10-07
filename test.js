9c7ef0eab8799db44ee4abf6fa63e411e2e3cb91
miftah1299 <jannat27120@gmail.com>
2024-10-08 02:36:04 +0600

    Refactor video.js and add getTimeString function

---
+++ test.js
@@ -0,0 +1,18 @@
+const isVerfied = "";
+
+console.log(`${isVerfied === true ? "verified" : "not verified"}`); // not verified
+
+//
+//
+//
+// time
+function getTimeString(time) {
+    // get hour and minute
+    const hour = Math.floor(time / 3600);
+    const minute = Math.floor((time % 3600) / 60);
+
+    // return time string
+    return `${hour}hrs ${minute}min ago`;
+}
+
+console.log(getTimeString(3729));
