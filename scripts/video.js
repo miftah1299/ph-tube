692fadaf31694f2c223927eb27e109f4db638d98
miftah1299 <jannat27120@gmail.com>
2024-10-08 00:48:43 +0600

    Added cardDemo for displaying videos

--- scripts/video.js
+++ scripts/video.js
@@ -12,10 +12,36 @@ const loadCategories = () => {
 const loadVideos = () => {
     fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
         .then((response) => response.json())
-        .then((data) => console.log(data))
+        .then((data) => DisplayVideos(data.videos))
         .catch((error) => console.log(error));
 };
 
+/*
+
+
+
+*/
+const cardDemo = {
+    category_id: "1001",
+    video_id: "aaaa",
+    thumbnail: "https://i.ibb.co/L1b6xSq/shape.jpg",
+    title: "Shape of You",
+    authors: [
+        {
+            profile_picture: "https://i.ibb.co/D9wWRM6/olivia.jpg",
+            profile_name: "Olivia Mitchell",
+            verified: "",
+        },
+    ],
+    others: {
+        views: "100K",
+        posted_date: "16278",
+    },
+    description:
+        "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey.",
+};
+
+// create DisplayVideos function
 const DisplayVideos = (videos) => {
     const videoContainer = document.getElementById("videos");
 
@@ -28,7 +54,7 @@ const DisplayVideos = (videos) => {
         card.innerHTML = `
         <figure>
             <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            src=${item.thumbnail}
             alt="Shoes" />
         </figure>
         <div class="card-body">
