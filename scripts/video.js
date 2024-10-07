f80f78c5cdb095c6f2f5b055f2dd0362bfda00ea
Your Name <jannat27120@gmail.com>
2024-10-08 02:36:04 +0600

    Refactor video.js and add getTimeString function

--- scripts/video.js
+++ scripts/video.js
@@ -1,3 +1,13 @@
+// time
+function getTimeString(time) {
+    // get hour and minute
+    const hour = Math.floor(time / 3600);
+    const minute = Math.floor((time % 3600) / 60);
+
+    // return time string
+    return `${hour}hrs ${minute}min ago`;
+}
+
 // fetch, load and show categories on the page
 
 // create loadCategories function
@@ -22,25 +32,25 @@ const loadVideos = () => {
 
 */
 // create cardDemo object
-const cardDemo = {
-    category_id: "1001",
-    video_id: "aaaa",
-    thumbnail: "https://i.ibb.co/L1b6xSq/shape.jpg",
-    title: "Shape of You",
-    authors: [
-        {
-            profile_picture: "https://i.ibb.co/D9wWRM6/olivia.jpg",
-            profile_name: "Olivia Mitchell",
-            verified: "",
-        },
-    ],
-    others: {
-        views: "100K",
-        posted_date: "16278",
-    },
-    description:
-        "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey.",
-};
+// const cardDemo = {
+//     category_id: "1001",
+//     video_id: "aaaa",
+//     thumbnail: "https://i.ibb.co/L1b6xSq/shape.jpg",
+//     title: "Shape of You",
+//     authors: [
+//         {
+//             profile_picture: "https://i.ibb.co/D9wWRM6/olivia.jpg",
+//             profile_name: "Olivia Mitchell",
+//             verified: "",
+//         },
+//     ],
+//     others: {
+//         views: "100K",
+//         posted_date: "16278",
+//     },
+//     description:
+//         "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey.",
+// };
 
 // create DisplayVideos function
 const DisplayVideos = (videos) => {
@@ -53,19 +63,35 @@ const DisplayVideos = (videos) => {
         const card = document.createElement("div");
         card.classList = "card card-compact";
         card.innerHTML = `
-        <figure class="h-[200px]">
+        <figure class="h-[200px] relative rounded-md">
             <img class="w-full h-full object-cover"
             src=${item.thumbnail}
-            alt="Shoes" />
+            alt="" />
+            ${
+                item.others.posted_date?.length === 0
+                    ? ""
+                    : `<span class="absolute bottom-2 right-2 bg-black bg-opacity-50 text-white px-2 py-1 rounded-md">
+                    ${getTimeString(item.others.posted_date)}</span>`
+            }
+        
         </figure>
         <div class="px-0 py-2 flex gap-2">
             <div class="flex items-center gap-2">
-                <img class="w-10 h-10 rounded-full"
+                <img class="w-10 h-10 rounded-full object-cover"
                 src=${item.authors[0].profile_picture}
                 alt="Olivia Mitchell" />
                 <div>
-                    <h3 class="font-semibold">${item.title}</h3>
-                    <p class="text-sm text-gray-500">${item.authors[0].profile_name}</p>
+                    <h3 class="font-bold">${item.title}</h3>
+                    <div class="flex gap-2 items-center">
+                        <p class="text-sm text-gray-500">${
+                            item.authors[0].profile_name
+                        }</p>
+                        ${
+                            item.authors[0].verified === true
+                                ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" alt="verified" />`
+                                : ""
+                        }
+                    </div>
                 </div>
             </div>
         </div>`;
